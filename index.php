eec0dd6a7074de0d18db79f52ad33c51ec93c680
Max <58748971+X86Max@users.noreply.github.com>
2022-07-23 10:47:56 -0300

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 if (isset($_POST['Email'])) {
 
     // EDIT THE 2 LINES BELOW AS REQUIRED
-    $email_to = ${{ secrets.EMAIL }};
+    $email_to = 'kickbuttowskithegame@gmail.com';
     $email_subject = "New form submissions";
 
     function problem($error)
