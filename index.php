29e1e38e9ab17df52b4fc1c42f5766c2073063fd
Max <58748971+X86Max@users.noreply.github.com>
2022-07-23 11:19:53 -0300

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 if (isset($_POST['email']) && !empty($_POST['email'])) {
   $subject = "New contact request: $_POST['subject']";
   $message = $_POST['body'];
-  $headers = 'From: info@website.com' . "\r\n" .
+  $headers = 'From:kickbuttowskithegame@gmail.com' . "\r\n" .
              'Reply-To: ' . $_POST['email']. "\r\n" .
              'X-Mailer: PHP/' . phpversion();
 
