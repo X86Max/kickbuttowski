309ddd09035733e7e48bd7d5ecfa85a8ad08282b
Max <58748971+X86Max@users.noreply.github.com>
2022-07-23 11:17:08 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,45 +1,12 @@
 <?php
+if (isset($_POST['email']) && !empty($_POST['email'])) {
+  $subject = "New contact request: $_POST['subject']";
+  $message = $_POST['body'];
+  $headers = 'From: info@website.com' . "\r\n" .
+             'Reply-To: ' . $_POST['email']. "\r\n" .
+             'X-Mailer: PHP/' . phpversion();
 
-#Receive user input
-$email_address = $_POST['email_address'];
-$feedback = $_POST['feedback'];
+  mail('kickbuttowskithegame@gmail.com', $subject, $message, $headers);
 
-#Filter user input
-function filter_email_header($form_field) {  
-return preg_replace('/[nr|!/<>^$%*&]+/','',$form_field);
+  die('Thank you for your email');
 }
-
-$email_address  = filter_email_header($email_address);
-
-#Send email
-$headers = "From: $email_addressn";
-$sent = mail('kickbuttowskithegame@gmail.com', 'Feedback Form Submission', $feedback, $headers);
-
-#Thank user or notify them of a problem
-if ($sent) {
-
-?><html>
-<head>
-<title>Thank You</title>
-</head>
-<body>
-<h1>Thank You</h1>
-<p>Thank you for your feedback.</p>
-</body>
-</html>
-<?php
-
-} else {
-
-?><html>
-<head>
-<title>Something went wrong</title>
-</head>
-<body>
-<h1>Something went wrong</h1>
-<p>We could not send your feedback. Please try again.</p>
-</body>
-</html>
-<?php
-}
-?>
